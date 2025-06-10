49d38654e9d8360f61c6e83e3d7ae2023ddcfa97
howardhung14 <howard11023331@gmail.com>
2025-06-10 12:26:12 +0800

    ball.py_v0.0

---
+++ 原神啟動/ball.py
@@ -0,0 +1,34 @@
+# ball.py
+import pygame
+import math
+
+class Ball:
+    def __init__(self, x, y, radius, hp):
+        self.x = x
+        self.y = y
+        self.radius = radius
+        self.hp = hp
+        self.dy = 2
+        self.font = pygame.font.SysFont("Arial", 24)  # 建立自己的字型物件
+
+    def move(self):
+        self.y += self.dy
+
+    def draw(self, win):
+        pygame.draw.circle(win, (0, 0, 0), (int(self.x), int(self.y)), self.radius)
+        text = self.font.render(str(self.hp), True, (255, 255, 255))
+        win.blit(text, (self.x - text.get_width() // 2, self.y - text.get_height() // 2))
+
+    def is_hit(self, bullet):
+        return math.hypot(self.x - bullet.x, self.y - bullet.y) < self.radius
+
+    def split(self):
+        if self.radius <= 15:
+            return []
+        new_radius = self.radius // 2
+        new_hp = self.hp // 2
+        return [
+            Ball(self.x - new_radius, self.y, new_radius, new_hp),
+            Ball(self.x + new_radius, self.y, new_radius, new_hp),
+        ]
+
