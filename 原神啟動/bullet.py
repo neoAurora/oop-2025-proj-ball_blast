f1c65dd8b8de63ef03f35108eb12e2f6354daa99
howardhung14 <howard11023331@gmail.com>
2025-06-10 21:13:59 +0800

    bullet.py_v1.0

--- 原神啟動/bullet.py
+++ 原神啟動/bullet.py
@@ -1,16 +1,14 @@
-# bullet.py
 import pygame
 
 class Bullet:
     def __init__(self, x, y):
         self.x = x
         self.y = y
-        self.radius = 5
-        self.speed = 10
+        self.radius = 7.5
+        self.speed = 6
 
     def move(self):
         self.y -= self.speed
 
     def draw(self, win):
         pygame.draw.circle(win, (255, 0, 0), (self.x, self.y), self.radius)
-
