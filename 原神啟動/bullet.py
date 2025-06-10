fc7dce3f0bcc4c25dabb01448f5bb834200c8413
howardhung14 <howard11023331@gmail.com>
2025-06-10 22:47:13 +0800

    add_bullet.jpg

--- 原神啟動/bullet.py
+++ 原神啟動/bullet.py
@@ -1,14 +1,24 @@
+# bullet.py
 import pygame
+import os
 
 class Bullet:
     def __init__(self, x, y):
         self.x = x
         self.y = y
-        self.radius = 7.5
-        self.speed = 6
+        self.speed = 10
+
+        img_path = os.path.join("bullet.jpg")  
+        self.image = pygame.image.load(img_path).convert()
+        self.image.set_colorkey((255, 255, 255))   #去除白色背景
+        self.image = pygame.transform.scale(self.image, (20, 20))
+        self.rect = self.image.get_rect(center=(self.x, self.y))
+        self.mask = pygame.mask.from_surface(self.image)
 
     def move(self):
         self.y -= self.speed
+        self.rect.center = (self.x, self.y)
 
     def draw(self, win):
-        pygame.draw.circle(win, (255, 0, 0), (self.x, self.y), self.radius)
+        win.blit(self.image, self.rect)
+
