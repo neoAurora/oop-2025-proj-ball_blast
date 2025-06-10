aa2b602548a6e42b1b29e18f440fb74eae567797
howardhung14 <howard11023331@gmail.com>
2025-06-10 12:49:06 +0800

    delete_hp=0_ball

--- 原神啟動/ball.py
+++ 原神啟動/ball.py
@@ -15,9 +15,10 @@ class Ball:
         self.y += self.dy
 
     def draw(self, win):
-        pygame.draw.circle(win, (0, 0, 0), (int(self.x), int(self.y)), self.radius)
-        text = self.font.render(str(self.hp), True, (255, 255, 255))
-        win.blit(text, (self.x - text.get_width() // 2, self.y - text.get_height() // 2))
+        if self.hp > 0:  # 只有HP大于0才绘制
+            pygame.draw.circle(win, (0, 0, 0), (int(self.x), int(self.y)), self.radius)
+            text = self.font.render(str(self.hp), True, (255, 255, 255))
+            win.blit(text, (self.x - text.get_width() // 2, self.y - text.get_height() // 2))
 
     def is_hit(self, bullet):
         return math.hypot(self.x - bullet.x, self.y - bullet.y) < self.radius
