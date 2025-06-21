f042991fa398ff9af7f8c50323f544140a67841e
Lawrence16428 <a0905201129@gmail.com>
2025-06-22 02:50:43 +0800

    modify_main

--- ball blast/main.py
+++ ball blast/main.py
@@ -15,7 +15,7 @@ from moviepy.editor import VideoFileClip
 pygame.init()
 
 # Screen settings
-SCREEN_WIDTH = 700
+SCREEN_WIDTH = 600
 SCREEN_HEIGHT = 800
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Ball Blast - OOP Project")
